0e2cd77537bfbb452ad2d4fbe966610676d352ad
1238 <1238@DESKTOP-CV0K8BU>
2022-12-23 17:44:42 +0900

    version 0.0.1

--- goodsList/src/ROOT/script/canvas.js
+++ goodsList/src/ROOT/script/canvas.js
@@ -9,6 +9,54 @@ window.addEventListener('load',()=>{
 	banner_1.src = '../images_source/banner_1.jpg';
 	banner_2.src = '../images_source/banner_2.jpg';
 	banner_3.src = '../images_source/banner_3.jpg';
+	const imgBanner = [
+		banner_1.src,
+		banner_2.src,
+		banner_3.src
+		
+	];
+	let imageQuantity = imgBanner.length;
+	
+	let time = 10;
+	async function setNumber(time){
+		return await new Promise((resolve, reject)=>{
+			setTimeout(()=>{
+				resolve(time);
+			}, time);
+		});
+	}
+	
+	async function blind(time, imgMum){
+		let cloneHTML = '';
+		let get = await setNumber(time);
+		console.log(0, imgMum);
+		cloneHTML += `
+			<section class="cloneBox" style="top:-350px; left:${0*50}px;"><img src="/images_source/banner_${imgMum}.jpg" style="top:0; left:-${0*50}px"></section>
+		`;
+		for(let i = 0 ; i < 23; i++){
+			get = await setNumber(get);
+			console.log(i+1, imgMum);
+			cloneHTML += `
+				<section class="cloneBox" style="top:-350px; left:${(i+1)*50}px;"><img src="/images_source/banner_${imgMum}.jpg" style="top:0; left:-${(i+1)*50}px"></section>
+			`;
+		}
+		mainBanner.innerHTML = cloneHTML;
+	}
+	
+	let collocateTime = 7000;
+	async function setCollocate(collocateTime){
+		let resetTime = await setNumber(collocateTime);
+		blind(time, 1);
+		for(let i = 0 ; i < imageQuantity-1; i++){
+			resetTime = await setNumber(resetTime);
+			blind(time, i+2);
+			if(i+1 == imageQuantity-1){
+				setCollocate(collocateTime);
+			}
+		}
+	}
+	setCollocate(collocateTime);
+/*
 	let init = 0;
 	let timeOut;
 	let imgCount = 2;
@@ -20,9 +68,9 @@ window.addEventListener('load',()=>{
 			`;
 		}
 		mainBanner.innerHTML = cloneHTML;
-		timeOut = setInterval(()=>{
-			effectImg(mainBanner, imgCount);
-		}, 70);
+		//timeOut = setInterval(()=>{
+			//effectImg(mainBanner, imgCount);
+		//}, 70);
 	}
 	setInterval(()=>{
 		cloneBox(`banner_${imgCount}`, imgCount);
@@ -30,6 +78,7 @@ window.addEventListener('load',()=>{
 		if(imgCount == 4)imgCount = 1;
 	}, 7000);
 	
+
 	function effectImg(ele, imgCount){
 		const cloneBox = ele.children;
 		cloneBox[init].style.top = `0px`;
@@ -43,4 +92,5 @@ window.addEventListener('load',()=>{
 			clearInterval(timeOut);
 		}
 	}
+	*/
 });
